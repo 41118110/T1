f49c1a969684901bd615b6c7ee436771b4d031ff
41118110 <148747989+41118110@users.noreply.github.com>
2023-12-25 15:17:53 +0800

    Add files via upload

---
+++ answer.js
@@ -0,0 +1,15 @@
+// 這是本題答案的原始 JavaScript 檔
+console.log('function adjustText() {
+  document.getElementById('student-id').style.color = 'blue';
+  document.getElementById('student-name').style.color = 'green';
+}
+
+function adjustImage() {
+  document.getElementById('profile-pic').style.border = '5px solid red';
+  document.getElementById('profile-pic').style.borderRadius = '20%';
+}
+
+function changeImage() {
+  document.getElementById('profile-pic').src = '41118110-2.jpg';
+}
+');
