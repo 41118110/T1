9d9a8b6d30412f438a67a80bf57b90b31ccaba1e
41118110 <148747989+41118110@users.noreply.github.com>
2023-12-25 15:05:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function adjustText() {
+  document.getElementById('student-id').style.color = 'blue';
+  document.getElementById('student-name').style.color = 'green';
+}
+
+function adjustImage() {
+  document.getElementById('profile-pic').style.border = '5px solid red';
+  document.getElementById('profile-pic').style.borderRadius = '20%';
+}
+
+function changeImage() {
+  document.getElementById('profile-pic').src = '41118110-2.jpg';
+}
